70a43a5c7cd5e5aee4a7d5c3f74c42754d6d805a
gunkims <45007556+kanpann@users.noreply.github.com>
2021-05-30 23:14:47 +0900

    User Entity, Repository 작성 및 테스트 코드 작성

---
+++ project-test/src/test/java/dev/gunlog/app/domain/UserRepositoryTests.java
@@ -0,0 +1,36 @@
+package dev.gunlog.app.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("이름으로 유저 정보 조회 테스트")
+    public void findByNameTest() {
+        String name = "gunkim";
+        int age = 22;
+        userRepository.save(User.builder()
+                .name(name)
+                .age(age)
+                .build());
+
+        User user = userRepository.findByName(name).get();
+
+        assertThat(user.getName(), is(equalTo(name)));
+        assertThat(user.getAge(), is(equalTo(age)));
+    }
+}
