27784aa3d727a848856fd7ba2676ab5c424870f5
gunkims <gunkim.dev@gmail.com>
2021-09-28 23:45:29 +0900

    feat: Person 및 공통 사항에 대한 Asciidoc 문서 구현

---
+++ asciidoc-sample/src/test/java/dev/gunlog/web/PersonControllerTests.java
@@ -0,0 +1,42 @@
+package dev.gunlog.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith({ SpringExtension.class, RestDocumentationExtension.class })
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class PersonControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+    @Test
+    public void test() throws Exception {
+        this.mockMvc.perform(get("/sayHello"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("person/sayHello"));
+    }
+}
