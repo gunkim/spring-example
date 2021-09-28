2c7ab037bdc7392173d87bf775781cede6803bd1
gunkims <gunkim.dev@gmail.com>
2021-09-29 03:28:28 +0900

    test: Path Parameter를 추가하여 빌드 테스트

--- asciidoc-sample/src/main/java/dev/gunlog/web/PersonController.java
+++ asciidoc-sample/src/main/java/dev/gunlog/web/PersonController.java
@@ -1,13 +1,17 @@
 package dev.gunlog.web;
 
+import dev.gunlog.model.Person;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v2/person")
 public class PersonController {
-    @GetMapping("/sayHello")
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Hello!");
+    @GetMapping(path="{name}")
+    public ResponseEntity<Person> gerPerson(@PathVariable String name) {
+        return ResponseEntity.ok(new Person(name, 0));
     }
 }
--- asciidoc-sample/src/test/java/dev/gunlog/web/PersonControllerTests.java
+++ asciidoc-sample/src/test/java/dev/gunlog/web/PersonControllerTests.java
@@ -8,18 +8,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static dev.gunlog.ApiDocumentUtils.getDocumentRequest;
-import static dev.gunlog.ApiDocumentUtils.getDocumentResponse;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @ExtendWith({ SpringExtension.class, RestDocumentationExtension.class })
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -36,11 +37,13 @@ public class PersonControllerTests {
     }
     @Test
     public void test() throws Exception {
-        this.mockMvc.perform(get("/sayHello"))
+        this.mockMvc.perform(RestDocumentationRequestBuilders.get("/api/v2/person/{name}", "gunkim"))
                 .andExpect(status().isOk())
                 .andDo(print())
-                .andDo(document("person/sayHello",
+                .andDo(document("person/gerPerson",
                         getDocumentRequest(),
-                        getDocumentResponse()));
+                        pathParameters(
+                                parameterWithName("name").description("사람 이름")
+                        )));
     }
 }
