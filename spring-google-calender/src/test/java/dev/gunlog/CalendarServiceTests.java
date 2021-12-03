8d9d8a3237edd4e686ac73c9dc9faf768fe6af51
gunkim <gunkim.dev@gmail.com>
2021-12-04 01:31:51 +0900

    테스트 코드 수정

--- spring-google-calender/src/test/java/dev/gunlog/CalendarServiceTests.java
+++ spring-google-calender/src/test/java/dev/gunlog/CalendarServiceTests.java
@@ -2,6 +2,7 @@ package dev.gunlog;
 
 import com.google.api.services.calendar.model.Event;
 import dev.gunlog.service.CalendarService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +20,15 @@ public class CalendarServiceTests {
     @Autowired
     private CalendarService calendarService;
     @Test
+    @DisplayName("오늘 일정 조회 테스트")
     void test() throws GeneralSecurityException, IOException {
-        List<Event> list = calendarService.test();
+        List<Event> list = calendarService.getNowSchedules();
 
         for (Event event : list) {
             System.out.println(event);
+        }
+        if(list.isEmpty()) {
+            System.out.println("오늘 일정은 없습니다.");
         }
     }
 }
